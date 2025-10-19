d139708b86ff2131299209330ce89aff76a89b82
Ved Badadhe <vbbadadhe@gmail.com>
2025-10-19 14:41:23 +0530

    Added Metrics Route

--- index.ts
+++ index.ts
@@ -1,22 +1,61 @@
 import express from "express";
 import { middleware } from "./middleware";
-
+import { NextFunction, Request, Response } from "express";
 const app = express();
+
 app.use(express.json());
 app.use(middleware);
+
+import client from "prom-client";
+
+const requestCounter = new client.Counter({
+  name: "http_requests_total",
+  help: "Total number of HTTP requests",
+  labelNames: ["method", "route", "status_code"], // this means dividing the data into the labels
+});
+
+export const requestCountMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const startTime = Date.now();
+
+  res.on("finish", () => {
+    const endTime = Date.now();
+    console.log(`Request took ${endTime - startTime}ms`);
+
+    // Increment request counter
+    requestCounter.inc({
+      method: req.method,
+      route: req.route ? req.route.path : req.path,
+      status_code: res.statusCode,
+    });
+  });
+
+  next();
+};
+app.use(requestCountMiddleware);
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
+app.get("/health", (req, res) => {
+  res.send("Health is OK !!");
+});
+
+app.get("/metrics", async (req, res) => {
+  const metrics = await client.register.metrics();
+  res.set("Content-Type", client.register.contentType);
+  res.end(metrics);
+});
+
 app.get("/cpu", (req, res) => {
-  const time = Date.now();
   for (let i = 0; i < 100000000; i++) {
     Math.random();
   }
 
-  res.send(
-    `CPU Intensive Task Completed time taken by it is ${Date.now() - time} ms`
-  );
+  res.send(`CPU Intensive Task Completed `);
 });
 
 const PORT = process.env.PORT || 3000;
